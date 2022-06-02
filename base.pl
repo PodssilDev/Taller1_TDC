ec0301189c7d7fb2b53282b26535931e21a5648e
John <john.serrano@usach.cl>
2022-06-01 22:25:18 -0400

    Consultas (con input) casi listas

--- base.pl
+++ base.pl
@@ -1,1 +1,17 @@
-song("Maroon 5 She will be loved", electronica, exponente, larga, letras_amor).
+/*
+Generos:
+Pop
+Metal
+Disco
+Hip-Hop
+Hard-Rock
+*/
+song("Maroon 5- She will be loved", pop, exponente, larga, moderna, banda).
+song("Michael Jackson - Thriller", disco, exponente, larga, antigua, cantante).
+song("Michael Jackson - Beat it", pop, exponente, larga, antigua, cantante).
+song("Billy Talent - Red Flag", rock, exponente, media, moderna, banda).
+song("Famous Last Words - One in the chamber", metal, underground, media, moderna, banda).
+song("Bee Gees - Stayin Alive", disco, exponente, larga, antigua, banda).
+song("The Weeknd - Blinding Lights", pop, exponente, media, moderna, cantante).
+
+
--- taller1.py
+++ taller1.py
@@ -1,27 +1,74 @@
+# BLOQUE DE DEFINICIONES
 # BLOQUE DE IMPORTACIONES
 import sys
 from pyswip import Prolog
 
+# BLOQUE PRINCIPAL
 prolog = Prolog()
 prolog.consult("base.pl")
 
-respuesta1 = input("Ingrese la duracion de la cancion: ")
-respuesta2 = input("Exponente o Underground?: ")
-respuesta3 = input("Ingrese una caracteristica extra: ")
+respuesta_1 = input("¿Prefiere un artista Exponente o Underground?: ")
+respuesta_2 = input("¿Prefiere una cancion larga, corta o media?: ")
+respuesta_3 = input("¿Prefiere una cancion de antes o despues de la decada de los 2000?:")
+respuesta_4 = input("Prefiere una banda o un cantante en solitario?:")
 
-if respuesta1 == "corta":
-    parametro1 = "corta"
-elif respuesta1 == "normal":
-    parametro1 = "normal"
-else:
-    parametro1 = "larga"
+no_encontrado = False
 
-consulta = list(prolog.query("song(J,_,exponente," + parametro1 + ", L)"))
-if consulta == []:
-    print("true")
+if respuesta_1 == "exponente":
+    tipo_artista = "exponente"
 else:
-    print("false")
-    print(consulta[0])
+    tipo_artista = "underground"
+
+if respuesta_2 == "corta":
+    duracion = "corta"
+
+elif respuesta_2 == "normal":
+    duracion = "normal"
+
+else:
+    duracion = "larga"
+
+if respuesta_3 == "antigua":
+    moderna_antigua = "antigua"
+
+else:
+    moderna_antigua = "moderna"
+
+if respuesta_4 == "banda":
+    solitario_grupo = "banda"
+else:
+    solitario_grupo = "cantante"
+
+# Consulta principal. Se buscan aquellas canciones que correspondan a las caracteristicas
+# elegidas por el usuario.
+
+lista_respuestas = list(prolog.query("song(Cancion,Genero," + tipo_artista + "," + duracion + "," + moderna_antigua + "," + solitario_grupo + ")"))
+
+# Es posible que no se encuentren resultados. En ese caso, se buscan canciones con 
+# caracteristicas similares (Utilizando variables anonimas)
+
+if(lista_respuestas == []):
+    no_encontrado = True
+    lista_respuestas = list(prolog.query("song(Cancion,Genero," + "_" + "," + duracion + "," + moderna_antigua + "," + solitario_grupo +")"))
+    if(lista_respuestas == []):
+        lista_respuestas = list(prolog.query("song(Cancion,Genero," + tipo_artista + "," + "_" + "," + moderna_antigua + "," + solitario_grupo +")"))
+        if(lista_respuestas == []):
+            no_encontrado = True
+            lista_respuestas = list(prolog.query("song(Cancion,Genero," + tipo_artista + "," + duracion + "," + "_" + "," + solitario_grupo +")"))
+            if(lista_respuestas == []):
+                no_encontrado = 1
+                lista_respuestas = list(prolog.query("song(Cancion,Genero," + tipo_artista + "," + duracion + "," + moderna_antigua + "," + "_" +")"))
+
+if lista_respuestas == []:
+    print("No hay respuestas")
+else:
+    if(no_encontrado == True):
+        print("No se encontraron canciones con las caracteristicas elegidas, pero los siguientes son similares: ")
+    else:
+        print("Se encontraron las siguientes canciones: ")
+    for respuesta in lista_respuestas:
+        print(respuesta["Cancion"])
+
 
 # Primero realizar las consultas, procurarse que funcione sin interfaz
 # Luego, realizar las consultas con interfaz
