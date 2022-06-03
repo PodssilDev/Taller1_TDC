bfb4988bbb09b8e5b3b538c8d6faa60a0adc92c7
John <john.serrano@usach.cl>
2022-06-03 00:25:11 -0400

    Mejoras en el codigo, base de conocimiento actualizada
    Falta terminar lo de la consulta 5, la cual permite determinar el genero. Lo dejo incompleto por ahora (a mimir)

--- base.pl
+++ base.pl
@@ -4,7 +4,7 @@ Pop
 Metal
 Disco
 Hip-Hop
-Hard-Rock
+Romantico
 Rock
 Jazz
 Blues
@@ -14,9 +14,9 @@ Techno
 Salsa
 Reggae
 Clasica
-Heavy Rock
+Cumbia
 Electronica
-6 por cada genero
+6 canciones por cada genero (como minimo)
 */
 /* POP*/
 song("Maroon 5- She will be loved", pop, exponente, larga, moderna, banda).
@@ -40,14 +40,33 @@ song("Metallica - Battery", metal, exponente, larga, antigua, banda).
 song("Iron Maiden - Run to the hills", metal, exponente, normal, antigua, banda).
 song("Famous Last Words - Welcome To The Show", metal, underground, normal, moderna, banda).
 song("The Amity Affliction - Pittsburgh", metal, exponente, normal, moderna, banda).
+song("Machine Head - Be Still And Know", metal, exponente, larga, moderna, banda).
+song("Scorpions - Rock You Like a Hurricane", metal, exponente, larga, antigua, banda).
 /* DISCO*/
 song("Michael Jackson - Thriller", disco, exponente, larga, antigua, cantante).
 song("Bee Gees - Stayin Alive", disco, exponente, larga, antigua, banda).
+song("Earth, Wind & Fire, The Emotions - Boogie Wonderland", disco, exponente, larga, antigua, banda).
+song("Bob Sinclar, Pitbull, Dragonfly, Fatman Scoop - Rock the Boat", disco, underground, normal, moderna, banda).
+song("Boney M. - Rasputin", disco, exponente, media, antigua, banda).
+song("KC & the Sunshine Band - Thats the Way", disco, exponente, normal, antigua, banda).
+song("ABBA - Gimme! Gimme! Gimme!", disco, exponente, larga, antigua, banda).
+song("Daft Punk, Pharrel Williams - Loose Yourself to Dance", disco, exponente, larga, moderna, banda).
+song("The Weeknd, Daft Punk - I Feel It Coming", disco, exponente, larga, moderna, banda).
+song("The Weeknd - In Your Eyes", disco, exponente, normal, moderna, cantante).
+
 /* ROCK */
 song("Billy Talent - Red Flag", rock, exponente, normal, moderna, banda).
 song("Phil Collins - In The Air Tonight", rock, exponente, larga, antigua, cantante).
 song("Autopilot Off - Clockworks", rock, exponente, normal, moderna, banda).
 song("Caesars - Jerk It Out", rock, exponente, normal, moderna, banda).
+song("Mike Oldfield - Nuclear", rock, exponente, larga, moderna, cantante).
+song("Brandon Yates - Olympus Mons", rock, underground, normal, moderna, cantante).
+
+/*JAZZ*/
+song("Shoji Meguro - Specialist", jazz, underground, normal, moderna, banda).
+song("Lyn - Beneath the Mask", jazz, underground, larga, moderna, banda).
+
+
 /* HIP-HOP */
 song("The Weeknd - Starboy", hiphop, exponente, normal, moderna, cantante).
 song("KSI - Creature", hiphop, underground, normal, moderna, cantante).
@@ -59,3 +78,16 @@ song("The Lonely Island - Like A Boss", hiphop, exponente, corta, moderna, banda
 /* ELECTRONICA */
 song("Skrillex - Rock n Roll", electronica, exponente, larga, moderna, cantante).
 song("Skrillex - Scary Monsters and Nice Sprites", electronica, exponente, larga, moderna, cantante).
+song("Martin Garrix - Animals", electronica, exponente, larga, moderna, cantante).
+song("LMFAO - Party Rock Anthem", electronica, exponente, larga, moderna, banda).
+song("New Order - Elegoia", electronica, exponente, larga, antigua, banda).
+song("Low Roar - Ill Keep Coming", electronica, exponente, larga, moderna, banda).
+song("Hello Venus - Im Ill", electronica, underground, normal, moderna, banda).
+
+/* Cumbia */
+song("Supermerk2 - La resaka", cumbia, exponente, normal, moderna, banda).
+
+/* Le mande la duda al ayudante si realmente son 16 generos como minimo. De todas formas
+falta seguir llenaod la base de conocimiento. El atributo de la caracteristica extra se
+agregara cuando esten listas todas las respuestas para todos los generos
+*/
--- taller1.py
+++ taller1.py
@@ -2,6 +2,12 @@
 # BLOQUE DE IMPORTACIONES
 import sys
 from pyswip import Prolog
+from statistics import mode
+
+# BLOQUE DE FUNCIONES
+
+def mas_repetido(lista_generos):
+    return(mode(lista_generos))
 
 # BLOQUE PRINCIPAL
 prolog = Prolog()
@@ -11,6 +17,7 @@ respuesta_1 = input("¿Prefiere un artista Exponente o Underground?: ")
 respuesta_2 = input("¿Prefiere una cancion larga, corta o normal?: ")
 respuesta_3 = input("¿Prefiere una cancion de antes o despues de la decada de los 2000?:")
 respuesta_4 = input("¿Prefiere una banda o un cantante en solitario?:")
+respuesta_5 = input("¿Prefiere una cancion con alguna caracteristica especifica?: ")
 
 no_encontrado = False # Se va a utilizar para el caso alternativo
 
@@ -39,6 +46,55 @@ if respuesta_4 == "banda":
 else:
     solitario_grupo = "cantante"
 
+'''
+La idea es que esta pregunta permita determinar con certeza el GENERO preferido
+No lo coloque aun en la base de conocimiento ya que faltan aun colocar las respuestas
+de algunos generos. Por mientras lo dejo asi por si alguien queire avanzar en algo mas
+Sigo apenas llegue a casa
+
+if respuesta_5 == "Que tenga letras de amor": # Romantica
+    caracteristica = "letras_amor"
+
+elif respuesta_5 == "Que tenga guitarras fuertes y distorcionadas": # Metal
+    caracteristica = "guitarras_fuertes"
+
+elif respuesta_5 == "Que tenga una preferencia por la artesania y diversos elementos": # POP
+    caracteristica = "artesania"
+
+elif respuesta_5 == "Que tenga sonidos electronicos": # Electronica
+    caracteristica = "sonidos_electronicos"
+
+elif respuesta_5 == "Que provoque querer bailar": # Disco
+    caracteristica = "querer_bailar"
+
+elif respuesta_5 == "Que contenga una letra hablada rapidamente": # Hip-Hop
+    caracteristica = "letras_rapidas"
+
+elif respuesta_5 == "Que tenga una letra en tono medio y una guitarra electronica":
+    caracteristica = "tono_medio"
+ 
+elif respuesta_5 == "Que tenga sonidos de saxophone": # Jazz
+    caracteristica = "sonidos_saxophone"
+
+
+Pop LISTO
+Metal LISTO
+Disco LISTO
+Hip-Hop LISTO
+Romantico LISTO
+Rock LISTO
+Jazz LISTO
+Blues
+Rock n Roll
+Country
+Techno
+Salsa
+Reggae
+Clasica
+Cumbia
+Electronica LISTO
+'''
+
 # Consulta principal. Se buscan aquellas canciones que correspondan a las caracteristicas
 # elegidas por el usuario.
 
@@ -59,6 +115,7 @@ if(lista_respuestas == []):
                 no_encontrado = 1
                 lista_respuestas = list(prolog.query("song(Cancion,Genero," + tipo_artista + "," + duracion + "," + moderna_antigua + "," + "_" +")"))
 
+lista_generos = []
 if lista_respuestas == []:
     print("No hay respuestas")
 else:
@@ -67,8 +124,10 @@ else:
     else:
         print("Se encontraron las siguientes canciones: ")
     for respuesta in lista_respuestas:
+        lista_generos.append(respuesta["Genero"])
         print(respuesta["Cancion"],"|", respuesta["Genero"])
 
+print(mas_repetido(lista_generos))
 
 # Primero realizar las consultas, procurarse que funcione sin interfaz
 # Luego, realizar las consultas con interfaz
