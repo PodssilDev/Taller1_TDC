e79bdf95e253ed3072f1e59428ededc0eb48699b
John <john.serrano@usach.cl>
2022-06-03 18:55:26 -0400

    Avances de hoy

--- base.pl
+++ base.pl
@@ -1,93 +1,129 @@
 /*
 Generos:
-Pop
-Metal
-Disco
-Hip-Hop
-Romantico
-Rock
-Jazz
-Blues
-Rock n Roll
-Country
-Techno
-Salsa
-Reggae
-Clasica
-Cumbia
-Electronica
+Pop LISTO
+Metal LISTO
+Disco LISTO
+Hip-Hop LISTO
+Romantico -> Daniel
+Rock-> Daniel
+Jazz LISTO
+Blues -> Daniel
+Rock n Roll ->Daniel
+Country -> Javier
+Techno -> Matías
+Salsa  -> Matías
+Reggae -> Javier
+Clasica LISTO
+Cumbia LISTO
+Electronica LISTO
 6 canciones por cada genero (como minimo)
 */
 /* POP*/
-song("Maroon 5- She will be loved", pop, exponente, larga, moderna, banda).
-song("Michael Jackson - Beat it", pop, exponente, larga, antigua, cantante).
-song("The Weeknd - Blinding Lights", pop, exponente, normal, moderna, cantante).
-song("Hunting high and low - Take on me", pop, exponente, normal, antigua, banda).
-song("The Chainsmokers - Closer", pop, exponente, larga, moderna, banda).
-song("Christian Kuria - Losing You", pop, underground, larga, moderna, cantante).
-song("Michael Jackson - Billie Jean", pop, exponente, larga, antigua, cantante).
-song("Frank Blunt - The Dirt Road", pop, underground, corta, moderna, cantante).
-song("BTS - Butter", pop, exponente, corta, moderna, banda).
-song("Kairi Yagi (Vivy) - Sing My Pleasure", pop, underground, larga, moderna, cantante).
-song("Yellowcard - Way Way", pop, exponente, normal, moderna, banda).
+song("Maroon 5- She will be loved", pop, exponente, larga, dosmil, banda, artesania_diversos_elementos).
+song("Michael Jackson - Beat it", pop, exponente, larga, ochentas, solista, artesania_diversos_elementos).
+song("The Weeknd - Blinding Lights", pop, exponente, normal, moderna, solista, artesania_diversos_elementos).
+song("Hunting high and low - Take on me", pop, exponente, normal, ochentas, banda, artesania_diversos_elementos).
+song("The Chainsmokers - Closer", pop, exponente, larga, moderna, banda, artesania_diversos_elementos).
+song("Christian Kuria - Losing You", pop, underground, larga, moderna, solista, artesania_diversos_elementos).
+song("Michael Jackson - Billie Jean", pop, exponente, larga, ochentas, solista, artesania_diversos_elementos).
+song("Frank Blunt - The Dirt Road", pop, underground, corta, moderna, solista, artesania_diversos_elementos).
+song("BTS - Butter", pop, exponente, corta, moderna, banda, artesania_diversos_elementos).
+song("Kairi Yagi (Vivy) - Sing My Pleasure", pop, underground, larga, moderna, solista, artesania_diversos_elementos).
+song("Yellowcard - Way Way", pop, exponente, normal, moderna, banda artesania_diversos_elementos).
 /* METAL*/
-song("Famous Last Words - One in the chamber", metal, underground, normal, moderna, banda).
-song("Beyond Borders - Absolute Zero", metal, underground, normal, moderna, banda).
-song("Pellek - Brokenbow", metal, exponente, normal, moderna, cantante).
-song("DragonForce - Through The Fire And Flames", metal, exponente, larga, moderna, banda).
-song("Babymetal - Gimme Chocolate!!", metal, underground, normal, moderna, banda).
-song("Metallica - Battery", metal, exponente, larga, antigua, banda).
-song("Iron Maiden - Run to the hills", metal, exponente, normal, antigua, banda).
-song("Famous Last Words - Welcome To The Show", metal, underground, normal, moderna, banda).
-song("The Amity Affliction - Pittsburgh", metal, exponente, normal, moderna, banda).
-song("Machine Head - Be Still And Know", metal, exponente, larga, moderna, banda).
-song("Scorpions - Rock You Like a Hurricane", metal, exponente, larga, antigua, banda).
+song("Famous Last Words - One in the chamber", metal, underground, normal, moderna, banda, guitarras_fuertes_distorcionadas).
+song("Beyond Borders - Absolute Zero", metal, underground, normal, moderna, banda, guitarras_fuertes_distorcionadas).
+song("Pellek - Brokenbow", metal, exponente, normal, moderna, solista, guitarras_fuertes_distorcionadas).
+song("DragonForce - Through The Fire And Flames", metal, exponente, dosmil, moderna, banda, guitarras_fuertes_distorcionadas).
+song("Babymetal - Gimme Chocolate!!", metal, underground, normal, moderna, banda, guitarras_fuertes_distorcionadas).
+song("Metallica - Battery", metal, exponente, larga, ochentas, banda, guitarras_fuertes_distorcionadas).
+song("Iron Maiden - Run to the hills", metal, exponente, normal, ochentas, banda, guitarras_fuertes_distorcionadas).
+song("Famous Last Words - Welcome To The Show", metal, underground, normal, moderna, banda, guitarras_fuertes_distorcionadas).
+song("The Amity Affliction - Pittsburgh", metal, exponente, normal, moderna, banda, guitarras_fuertes_distorcionadas).
+song("Machine Head - Be Still And Know", metal, exponente, larga, moderna, banda, guitarras_fuertes_distorcionadas).
+song("Scorpions - Rock You Like a Hurricane", metal, exponente, larga, ochentas, banda, guitarras_fuertes_distorcionadas).
 /* DISCO*/
-song("Michael Jackson - Thriller", disco, exponente, larga, antigua, cantante).
-song("Bee Gees - Stayin Alive", disco, exponente, larga, antigua, banda).
-song("Earth, Wind & Fire, The Emotions - Boogie Wonderland", disco, exponente, larga, antigua, banda).
-song("Bob Sinclar, Pitbull, Dragonfly, Fatman Scoop - Rock the Boat", disco, underground, normal, moderna, banda).
-song("Boney M. - Rasputin", disco, exponente, media, antigua, banda).
-song("KC & the Sunshine Band - Thats the Way", disco, exponente, normal, antigua, banda).
-song("ABBA - Gimme! Gimme! Gimme!", disco, exponente, larga, antigua, banda).
-song("Daft Punk, Pharrel Williams - Loose Yourself to Dance", disco, exponente, larga, moderna, banda).
-song("The Weeknd, Daft Punk - I Feel It Coming", disco, exponente, larga, moderna, banda).
-song("The Weeknd - In Your Eyes", disco, exponente, normal, moderna, cantante).
+song("Michael Jackson - Thriller", disco, exponente, larga, ochentas, solista, querer_bailar).
+song("Bee Gees - Stayin Alive", disco, exponente, larga, setentas, banda, querer_bailar).
+song("Earth, Wind & Fire, The Emotions - Boogie Wonderland", disco, exponente, larga, setentas, banda, querer_bailar).
+song("Bob Sinclar, Pitbull, Dragonfly, Fatman Scoop - Rock the Boat", disco, underground, normal, moderna, banda, querer_bailar).
+song("Boney M. - Rasputin", disco, exponente, media, setentas, banda, querer_bailar).
+song("KC & the Sunshine Band - Thats the Way", disco, exponente, normal, setentas, banda, querer_bailar).
+song("ABBA - Gimme! Gimme! Gimme!", disco, exponente, larga, setentas, banda, querer_bailar).
+song("Daft Punk, Pharrel Williams - Loose Yourself to Dance", disco, exponente, larga, moderna, banda, querer_bailar).
+song("The Weeknd, Daft Punk - I Feel It Coming", disco, exponente, larga, moderna, banda, querer_bailar).
+song("The Weeknd - In Your Eyes", disco, exponente, normal, moderna, solista, querer_bailar).
 
 /* ROCK */
-song("Billy Talent - Red Flag", rock, exponente, normal, moderna, banda).
-song("Phil Collins - In The Air Tonight", rock, exponente, larga, antigua, cantante).
-song("Autopilot Off - Clockworks", rock, exponente, normal, moderna, banda).
-song("Caesars - Jerk It Out", rock, exponente, normal, moderna, banda).
-song("Mike Oldfield - Nuclear", rock, exponente, larga, moderna, cantante).
-song("Brandon Yates - Olympus Mons", rock, underground, normal, moderna, cantante).
+song("Billy Talent - Red Flag", rock, exponente, normal, dosmil, banda, tono_medio_guitarra).
+song("Phil Collins - In The Air Tonight", rock, exponente, larga, ochentas, solista, tono_medio_guitarra).
+song("Autopilot Off - Clockworks", rock, exponente, normal, dosmil, banda, tono_medio_guitarra).
+song("Caesars - Jerk It Out", rock, exponente, normal, dosmil, banda, tono_medio_guitarra).
+song("Mike Oldfield - Nuclear", rock, exponente, larga, moderna, solista, tono_medio_guitarra).
+song("Brandon Yates - Olympus Mons", rock, underground, normal, moderna, solista, tono_medio_guitarra).
 
 /*JAZZ*/
-song("Shoji Meguro - Specialist", jazz, underground, normal, moderna, banda).
-song("Lyn - Beneath the Mask", jazz, underground, larga, moderna, banda).
-
+song("Shoji Meguro - Specialist", jazz, underground, normal, dosmil, banda, sonidos_saxophone).
+song("Lyn - Beneath the Mask", jazz, underground, larga, moderna, banda, sonidos_saxophone).
+song("Frank Sinatra - You Go to My Head", jazz, exponente, larga, antigua, solista, sonidos_saxophone).
+song("Frank Sinatra - Fly Me to the Moon", jazz, exponente, corta, antigua, solista, sonidos_saxophone).
+song("John Coltrane - Wis One", jazz, exponente, larga, antigua, solista, sonidos_saxophone).
+song("Kamasi Washington - The Rhythm Changes", jazz, underground, larga, moderna, solista, sonidos_saxophone).
 
 /* HIP-HOP */
-song("The Weeknd - Starboy", hiphop, exponente, normal, moderna, cantante).
-song("KSI - Creature", hiphop, underground, normal, moderna, cantante).
-song("Jaden - On My Own", hiphop, exponente, normal, moderna, cantante).
-song("Run-DMC - Its Tricky", hiphop, exponente, normal, antigua, banda).
-song("The Lonely Island - Im on a Boat (Ft. T-Pain)", hiphop, exponente, corta, moderna, banda).
-song("The Lonely Island - Like A Boss", hiphop, exponente, corta, moderna, banda).
+song("The Weeknd - Starboy", hiphop, exponente, normal, moderna, solista, letras_rapidas).
+song("KSI - Creature", hiphop, underground, normal, moderna, solista, letras_rapidas).
+song("Jaden - On My Own", hiphop, exponente, normal, moderna, solista, letras_rapidas).
+song("Run-DMC - Its Tricky", hiphop, exponente, normal, ochentas, banda, letras_rapidas).
+song("The Lonely Island - Im on a Boat (Ft. T-Pain)", hiphop, exponente, corta, dosmil, banda, letras_rapidas).
+song("The Lonely Island - Like A Boss", hiphop, exponente, corta, dosmil, banda, letras_rapidas).
 
 /* ELECTRONICA */
-song("Skrillex - Rock n Roll", electronica, exponente, larga, moderna, cantante).
-song("Skrillex - Scary Monsters and Nice Sprites", electronica, exponente, larga, moderna, cantante).
-song("Martin Garrix - Animals", electronica, exponente, larga, moderna, cantante).
-song("LMFAO - Party Rock Anthem", electronica, exponente, larga, moderna, banda).
-song("New Order - Elegoia", electronica, exponente, larga, antigua, banda).
-song("Low Roar - Ill Keep Coming", electronica, exponente, larga, moderna, banda).
-song("Hello Venus - Im Ill", electronica, underground, normal, moderna, banda).
+song("Skrillex - Rock n Roll", electronica, exponente, larga, moderna, solista, sonidos_electronicos).
+song("Skrillex - Scary Monsters and Nice Sprites", electronica, exponente, larga, moderna, solista, sonidos_electronicos).
+song("Martin Garrix - Animals", electronica, exponente, larga, moderna, solista, sonidos_electronicos).
+song("LMFAO - Party Rock Anthem", electronica, exponente, larga, moderna, banda, sonidos_electronicos).
+song("New Order - Elegoia", electronica, exponente, larga, ochentas, banda, sonidos_electronicos).
+song("Low Roar - Ill Keep Coming", electronica, exponente, larga, moderna, banda, sonidos_electronicos).
+song("Hello Venus - Im Ill", electronica, underground, normal, moderna, banda, sonidos_electronicos).
 
 /* Cumbia */
-song("Supermerk2 - La resaka", cumbia, exponente, normal, moderna, banda).
+song("Supermerk2 - La resaka", cumbia, exponente, normal, dosmil, banda, ritmos_movidos).
+song("Chico Trujillo - Conductor", cumbia, exponente, corta, dosmil, banda, ritmos_movidos).
+song("Los Vikings 5 - Boquita de Caramelo", cumbia, exponente, normal, dosmil, banda, ritmos_movidos).
+song("Noche de Brujas - Debería Odiarte", cumbia, exponente, normal, dosmil, banda, ritmos_movidos).
+song("Luisko y la Fruta Fresca - Irme a Neptuno", cumbia, underground, larga, moderna, banda, ritmos_movidos).
+song("Leo Rey - Mortal Kumbiat", cumbia, exponente, normal, moderna, solista, ritmos_movidos).
 
-/* Le mande la duda al ayudante si realmente son 16 generos como minimo. De todas formas
-falta seguir llenaod la base de conocimiento. El atributo de la caracteristica extra se
-agregara cuando esten listas todas las respuestas para todos los generos
-*/
+/* Clasica (8) */
+song("Ludiwg Beethoven - Sonata per il piano n°12 Quasi una Fantasia. Opera 27,. N°2", clasica, exponente, larga, antigua, banda, sinfonia).
+song("Georg Handel - Messiah HWV 53", clasica, exponente, larga, antigua, banda, sinfonia).
+song("Amadeus Mozart - Serenade No. 13 for strings in G major, K. 525", clasica, exponente, larga, antigua, banda, sinfonia).
+song("Piotr Chaikovski - 1812 overture. Op 49", clasica, exponente, larga, antigua, banda, sinfonia).
+song("Edvard Grieg - Peer Gynt Opus 23", clasica, exponente, larga, antigua, banda, sinfonia).
+song("Gioachino Rossini - William Tell overture", clasica, exponente, larga, antigua, banda, sinfonia).
+song("Bach - Cello suite No. 4 in E-flat major", clasica, exponente, larga, antigua, solista, sinfonia).
+song("Gaspar Cassado - Suite for Cello Solo", clasica, exponente, larga, antigua, solista, sinfonia).
+
+/* Country */
+song("John Denver - Take Me Home, Country Roads", country, exponente, normal, setentas, solista, armonias_vocales).
+song("Dolly Parton - Jolene", country, underground, corta, setentas, solista, armonias_vocales).
+song("Willie Nelson - Always In My Mind", country, exponente, normal, , solista, armonias_vocales).
+song("Carter Family - Will The Circle Be Unbroken", country, underground, normal, antigua, banda, armonias_vocales).
+song("Patsy Montana - I Wanna Be A Cowboy Sweetheart", country, exponente, normal, antigua, solista, armonias_vocales).
+
+
+
+
+/* Techno */
+song("Green Velvet - Flash", techno, underground, larga, noventas, solista, tempo_rapido)
+song("Adam Beyer & Bart Skils - Your Mind", techno, underground, larga, moderna, banda, tempo_rapido)
+song("Niels Van Gogh - Pulverturm (Denise schneider remix)", techno, underground, larga, moderna, solista, tempo_rapido)
+song("D.K..O x KNNY x SHAW - I Feel", techno, underground, corta, moderna, banda, tempo_rapido)
+song(nombre, techno, tipoArt, duracion, decada, solis-banda, tempo_rapido)
+song(nombre, techno, tipoArt, duracion, decada, solis-banda, tempo_rapido)
+
+
+/* Blues */
+
+song("John Lee Hooker - Boom Boom", blues, exponente, larga, )
--- taller1.py
+++ taller1.py
@@ -3,16 +3,146 @@
 import sys
 from pyswip import Prolog
 from statistics import mode
+from cProfile import label
+import tkinter as tk
+from tkinter import BOTTOM, filedialog, Text
+import os
+
+#Bloque de Listas
+#----------------------------------------------------------------------------
+#Listas de opciones
+opciones1 = ["Exponente","Underground"]
+opciones2 = ["Duración corta","Duración Media","Duración Larga"]
+opciones3 = ["Moderna","Dosmil","Noventas","Onchentas","Sententas","Clasica"]
+opciones4 = ["Solista","Banda"]
+opciones5 = ["Que tenga letras de amor"
+            ,"Que tenga guitarras fuertes y distorcionadas"
+            ,"Que tenga una preferencia por la artesania y diversos elementos"
+            ,"Que tenga sonidos electronicos"
+            ,"Que provoque querer bailar"
+            ,"Que contenga una letra hablada rapidamente"
+            ,"Que tenga una letra en tono medio y una guitarra electronica"
+            ,"Que tenga sonidos de saxophone"
+            ,"Que tenga sonidos de una bateria ritmica"
+            ,"Que tenga ritmos bastante movidos"
+            ,"Que tenga sinfonia y tengan poca o nula improvisacion"
+            ,"Que tenga tempo rapido y sonidos distorcionados"
+            ,"Que use el patron ritmico del son cubano"
+            ,"Que tenga un ritmo de musica bailable y frenetico"
+            ,"Que tenga tecnicas expresivas de la guitarra y harmonia"
+            ,"Que emplee armonias vocales"
+            ,"Ninguna de las anteriores"]
+
+
+#Listas que Contienen las opciones
+Lista_suprema = [opciones1, opciones2, opciones3, opciones4]
+
+Lista = ["Reconocimiento","Duración","Epoca","Cantante o Banda","Caracteristicas Extras"]
+Lista_de_entradas = []
+Lista_de_salida = []
+#----------------------------------------------------------------------------
 
 # BLOQUE DE FUNCIONES
+#----------------------------------------------------------------------------
+
+def reconocer(caso):
+    match caso:
+        case "Que tenga letras de amor": # Romantica
+            return "letras_de_amor"
+
+        case "Que tenga guitarras fuertes y distorcionadas": # Metal
+            return "guitarras_fuertes_distorcionadas"
+
+        case "Que tenga una preferencia por la artesania y diversos elementos": # POp
+            return "artesania_diversos_elementos" 
+
+        case "Que tenga sonidos electronicos": # Electronica
+            return "sonidos_electronicos"
+
+        case "Que provoque querer bailar": # Disco
+            return "querer_bailar"
+
+        case "Que contenga una letra hablada rapidamente": #Hip-Hop
+            return "letras_rapidas"
+
+        case "Que tenga una letra en tono medio y una guitarra electronica": # Rock
+            return "tono_medio_guitarra"
+
+        case "Que tenga sonidos de saxophone": # Jazz
+            return "sonidos_saxophone"
+
+        case "Que tenga sonidos de una bateria ritmica": # Reggae
+            return "sonidos_bateria" 
+
+        case "Que tenga ritmos bastante movidos": #  Cumbia
+            return "ritmos_movidos"
+
+        case "Que tenga sinfonia y tengan poca o nula improvisacion": # Clasica
+            return "sinfonia"
+
+        case "Que tenga tempo rapido y sonidos distorcionados": # Tecno
+            return "tempo_rapido"
+
+        case "Que use el patron ritmico del son cubano": # Salsa
+            return "patron_cubano"
+
+        case "Que tenga un ritmo de musica bailable y frenetico": # Rock n Roll
+            return "ritmo_bailable_frenetico"
+
+        case "Que tenga tecnicas expresivas de la guitarra y harmonia":
+            return "tecnicas_expresivas" # Blues
+
+        case "Que emplee armonias vocales": # Country
+            return "armonias_vocales"
+
+        case "Ninguna de las anteriores":
+            return "_"
+        
+def recivir():
+    for clicked1 in Lista_de_entradas:
+        Lista_de_salida.append(clicked1.get())
+    #Lista_de_salida.append(entradas.get())
 
 def mas_repetido(lista_generos):
     return(mode(lista_generos))
+
+def songQuery(tipo_artista, duracion, moderna_antigua, banda_solista):
+    return list(prolog.query(f"song(Cancion, Genero, {tipo_artista}, {duracion}, {moderna_antigua}, {banda_solista})"))
+
+
+def generoMasRepetido(respuestas):
+    return mode([r["Genero"] for r in respuestas])
+
+def cancionesGenero(respuestas, genero):
+    return [r["Cancion"] for r in respuestas if r["Genero"] == genero]
+
+def gustosSimilares(datos):
+    respuestas = []
+    for i in range(0, len(datos)):
+        if(respuestas != []):
+            return respuestas
+        d = datos
+        d[i] = "_"      
+        respuestas = songQuery(d[0], d[1], d[2], d[3])
+
+def entrega_cancion(V, Genero, lista_cancion):
+    if(V):
+        entrega = "El Genero Encontrado: " + Genero +"\n"
+        for i in range(lista_cancion):
+            entrega = entrega +  str(i+1) + ".-" + lista_cancion[i] + "\n" 
+    else:
+        entrega = "No se encontraron preferencias, pero se recomienda el genero: " + Genero + "\n"
+        for i in range(lista_cancion):
+            entrega = entrega + str(i+1) + ".-" + lista_cancion[i] + "\n" 
+    return entrega
+
+#----------------------------------------------------------------------------
 
 # BLOQUE PRINCIPAL
 prolog = Prolog()
 prolog.consult("base.pl")
 
+'''
 respuesta_1 = input("¿Prefiere un artista Exponente o Underground?: ")
 respuesta_2 = input("¿Prefiere una cancion larga, corta o normal?: ")
 respuesta_3 = input("¿Prefiere una cancion de antes o despues de la decada de los 2000?:")
@@ -35,7 +165,7 @@ elif respuesta_2 == "normal":
 else:
     duracion = "larga"
 
-if respuesta_3 == "antigua":
+if respuesta_3 == "antigua": # Moderna, Dosmil, Noventas, Ochentas, Setentas, Clasica
     moderna_antigua = "antigua"
 
 else:
@@ -45,37 +175,13 @@ if respuesta_4 == "banda":
     solitario_grupo = "banda"
 else:
     solitario_grupo = "cantante"
+'''
 
 '''
 La idea es que esta pregunta permita determinar con certeza el GENERO preferido
 No lo coloque aun en la base de conocimiento ya que faltan aun colocar las respuestas
 de algunos generos. Por mientras lo dejo asi por si alguien queire avanzar en algo mas
 Sigo apenas llegue a casa
-
-if respuesta_5 == "Que tenga letras de amor": # Romantica
-    caracteristica = "letras_amor"
-
-elif respuesta_5 == "Que tenga guitarras fuertes y distorcionadas": # Metal
-    caracteristica = "guitarras_fuertes"
-
-elif respuesta_5 == "Que tenga una preferencia por la artesania y diversos elementos": # POP
-    caracteristica = "artesania"
-
-elif respuesta_5 == "Que tenga sonidos electronicos": # Electronica
-    caracteristica = "sonidos_electronicos"
-
-elif respuesta_5 == "Que provoque querer bailar": # Disco
-    caracteristica = "querer_bailar"
-
-elif respuesta_5 == "Que contenga una letra hablada rapidamente": # Hip-Hop
-    caracteristica = "letras_rapidas"
-
-elif respuesta_5 == "Que tenga una letra en tono medio y una guitarra electronica":
-    caracteristica = "tono_medio"
- 
-elif respuesta_5 == "Que tenga sonidos de saxophone": # Jazz
-    caracteristica = "sonidos_saxophone"
-
 
 Pop LISTO
 Metal LISTO
@@ -84,14 +190,14 @@ Hip-Hop LISTO
 Romantico LISTO
 Rock LISTO
 Jazz LISTO
-Blues
-Rock n Roll
-Country
-Techno
-Salsa
-Reggae
-Clasica
-Cumbia
+Blues LISTO
+Rock n Roll LISTO
+Country LISTO
+Techno LISTO
+Salsa LISTO
+Reggae LISTO
+Clasica LISTO
+Cumbia LISTO 
 Electronica LISTO
 '''
 
@@ -129,5 +235,65 @@ else:
 
 print(mas_repetido(lista_generos))
 
+
+#BLoque de la aplicación
+#--------------------------------------------------------------------------------------
+
+#La pantalla del Programa
+root = tk.Tk()
+root.geometry("1200x500")
+
+#El fondo del programa
+canvas = tk.Canvas(root , bg="#EA7600")
+canvas.place(relwidth=1, relheight=1)
+
+#lugar donde estan las opciones
+frame = tk.Frame(root, bg="#002F6C")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+#Titulo de la encuesta
+titulo = tk.Label(frame, text = "Prieba del Taller de TDC", bg="#B1B1B1" , width=67)
+titulo.grid(padx=2 , pady=2, row = 0 , column = 0 , sticky=tk.E+tk.W)
+
+#Nombre de que entrar en la encuesta
+for i in range(len(Lista)):
+    label1 = tk.Label(frame , text= Lista[i] , bg="#B1B1B1")
+    primo = (2*i)+1
+    label1.grid(padx=2 , pady=2, row = primo , column = 0 , sticky=tk.E+tk.W)
+
+#las opciones que puedes eleguir en la encuesta
+for i in range(len(Lista_suprema)):
+    clicked1 = tk.StringVar()
+    clicked1.set(Lista_suprema[i][0])
+    abrir1 = tk.OptionMenu(frame, clicked1, *Lista_suprema[i])
+    abrir1.grid(padx=2 , pady=2, row = (2*i)+2 , column = 0 , sticky=tk.E+tk.W)
+    Lista_de_entradas.append(clicked1)
+
+#etiqueta que nombra donde puedes escribir en la encuesta
+label2 = tk.Label(frame, text="Pequeña descripcion de lo que quiere.", bg="#B1B1B1")
+label2.grid(padx=2 , pady=2,row=(2*len(Lista))+3, columnspan=2 , sticky=tk.E+tk.W)
+
+
+#entrada de la encuesta ahora no utilizada
+'''
+entradas = tk.Entry(frame, justify="center")
+entradas.grid(row=(2*len(Lista))+4, columnspan=2, ipadx=10, ipady=10 ,sticky=tk.E+tk.W)
+'''
+
+#boton que guarda las encuesta
+summit = tk.Button(frame, text="Summit", padx=10, pady=5, fg = "black", bg = "white", command=recivir)
+summit.grid(padx=2 , pady=2,row=(2*len(Lista))+4, columnspan=2)
+
+
+#aqui agrgar una funcion que cuando encuentre una respuesta la escriba sobre esto
+
+#salida_texto = entrega_cancion(no_encontrado, Genero, Lista_canciones) #modificar para que entrege listo el texto 
+salida_texto = "Salida"
+
+salida= tk.Label(frame, text= salida_texto, bg = "white", width=67)
+salida.grid( row=0, rowspan=(2*len(Lista))+5, column = 2, sticky=tk.N ,ipadx = 10,ipady = 10)
+
+root.mainloop()
+#----------------------------------------------------------------------------
 # Primero realizar las consultas, procurarse que funcione sin interfaz
 # Luego, realizar las consultas con interfaz
