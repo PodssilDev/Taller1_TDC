387acfb0792357284bf947abacb3ca59eb354d36
John <john.serrano@usach.cl>
2022-06-03 22:05:45 -0400

    Base de conocimientos terminada

--- base.pl
+++ base.pl
@@ -1,24 +1,17 @@
-/*
-Generos:
-Pop LISTO
-Metal LISTO
-Disco LISTO
-Hip-Hop LISTO
-Romantico -> Daniel
-Rock-> Daniel
-Jazz LISTO
-Blues -> Daniel
-Rock n Roll ->Daniel
-Country -> Javier
-Techno -> Matías
-Salsa  -> Matías
-Reggae -> Javier
-Clasica LISTO
-Cumbia LISTO
-Electronica LISTO
-6 canciones por cada genero (como minimo)
-*/
-/* POP*/
+% Generos: Pop - Metal - Disco - Hip-Hop - Romantico - Rock - Jazz - Blues - Rock n Roll - Country
+% Techno - Salsa - Reggae - Clasica - Cumbia - Electronica
+
+% Dominio:
+% Cancion: String
+% Genero, Tipo_Artista, Duracion, Decada, Solista_Banda, Caracteristica: Atomo
+% Metas Primarias: song
+% Predicados:
+% song(Cancion, Genero, Tipo_Artista, Duracion, Decada, Solista_Banda, Caracteristica). (Aridad = 7)
+
+% Clausulas
+% Hechos:
+
+% ----------------------------------------------------POP------------------------------------------------
 song("Maroon 5- She will be loved", pop, exponente, larga, dosmil, banda, artesania_diversos_elementos).
 song("Michael Jackson - Beat it", pop, exponente, larga, ochentas, solista, artesania_diversos_elementos).
 song("The Weeknd - Blinding Lights", pop, exponente, normal, moderna, solista, artesania_diversos_elementos).
@@ -29,8 +22,8 @@ song("Michael Jackson - Billie Jean", pop, exponente, larga, ochentas, solista, 
 song("Frank Blunt - The Dirt Road", pop, underground, corta, moderna, solista, artesania_diversos_elementos).
 song("BTS - Butter", pop, exponente, corta, moderna, banda, artesania_diversos_elementos).
 song("Kairi Yagi (Vivy) - Sing My Pleasure", pop, underground, larga, moderna, solista, artesania_diversos_elementos).
-song("Yellowcard - Way Way", pop, exponente, normal, moderna, banda artesania_diversos_elementos).
-/* METAL*/
+song("Yellowcard - Way Way", pop, exponente, normal, moderna, banda, artesania_diversos_elementos).
+% ---------------------------------------------------METAL------------------------------------------------
 song("Famous Last Words - One in the chamber", metal, underground, normal, moderna, banda, guitarras_fuertes_distorcionadas).
 song("Beyond Borders - Absolute Zero", metal, underground, normal, moderna, banda, guitarras_fuertes_distorcionadas).
 song("Pellek - Brokenbow", metal, exponente, normal, moderna, solista, guitarras_fuertes_distorcionadas).
@@ -42,7 +35,7 @@ song("Famous Last Words - Welcome To The Show", metal, underground, normal, mode
 song("The Amity Affliction - Pittsburgh", metal, exponente, normal, moderna, banda, guitarras_fuertes_distorcionadas).
 song("Machine Head - Be Still And Know", metal, exponente, larga, moderna, banda, guitarras_fuertes_distorcionadas).
 song("Scorpions - Rock You Like a Hurricane", metal, exponente, larga, ochentas, banda, guitarras_fuertes_distorcionadas).
-/* DISCO*/
+% ---------------------------------------------------DISCO------------------------------------------------
 song("Michael Jackson - Thriller", disco, exponente, larga, ochentas, solista, querer_bailar).
 song("Bee Gees - Stayin Alive", disco, exponente, larga, setentas, banda, querer_bailar).
 song("Earth, Wind & Fire, The Emotions - Boogie Wonderland", disco, exponente, larga, setentas, banda, querer_bailar).
@@ -53,32 +46,34 @@ song("ABBA - Gimme! Gimme! Gimme!", disco, exponente, larga, setentas, banda, qu
 song("Daft Punk, Pharrel Williams - Loose Yourself to Dance", disco, exponente, larga, moderna, banda, querer_bailar).
 song("The Weeknd, Daft Punk - I Feel It Coming", disco, exponente, larga, moderna, banda, querer_bailar).
 song("The Weeknd - In Your Eyes", disco, exponente, normal, moderna, solista, querer_bailar).
-
-/* ROCK */
+% ---------------------------------------------------ROCK------------------------------------------------
 song("Billy Talent - Red Flag", rock, exponente, normal, dosmil, banda, tono_medio_guitarra).
 song("Phil Collins - In The Air Tonight", rock, exponente, larga, ochentas, solista, tono_medio_guitarra).
 song("Autopilot Off - Clockworks", rock, exponente, normal, dosmil, banda, tono_medio_guitarra).
 song("Caesars - Jerk It Out", rock, exponente, normal, dosmil, banda, tono_medio_guitarra).
 song("Mike Oldfield - Nuclear", rock, exponente, larga, moderna, solista, tono_medio_guitarra).
 song("Brandon Yates - Olympus Mons", rock, underground, normal, moderna, solista, tono_medio_guitarra).
-
-/*JAZZ*/
+song("Nirvana - Come as you are", rock, exponente, normal, noventas, banda, tono_medio_guitarra).
+song("Wolfmother - Woman", rock, underground, corta, dosmil, banda, tono_medio_guitarra).
+song("Set It Off - Wolf In Sheeps Clothing", rock, exponente, normal, moderna, banda, tono_medio_guitarra).
+song("The Who - Pinball Wizard", rock, exponente, normal, antigua, banda, tono_medio_guitarra).
+song("The Peggies - FootPrints", rock, exponente, normal, moderna, banda, tono_medio_guitarra).
+song("Sambomaster - Seishun kyousoukyoku", rock, exponente, larga, dosmil, banda, tono_medio_guitarra).
+% ---------------------------------------------------JAZZ------------------------------------------------
 song("Shoji Meguro - Specialist", jazz, underground, normal, dosmil, banda, sonidos_saxophone).
 song("Lyn - Beneath the Mask", jazz, underground, larga, moderna, banda, sonidos_saxophone).
 song("Frank Sinatra - You Go to My Head", jazz, exponente, larga, antigua, solista, sonidos_saxophone).
 song("Frank Sinatra - Fly Me to the Moon", jazz, exponente, corta, antigua, solista, sonidos_saxophone).
 song("John Coltrane - Wis One", jazz, exponente, larga, antigua, solista, sonidos_saxophone).
 song("Kamasi Washington - The Rhythm Changes", jazz, underground, larga, moderna, solista, sonidos_saxophone).
-
-/* HIP-HOP */
+%  -------------------------------------------------HIP-HOP----------------------------------------------
 song("The Weeknd - Starboy", hiphop, exponente, normal, moderna, solista, letras_rapidas).
 song("KSI - Creature", hiphop, underground, normal, moderna, solista, letras_rapidas).
 song("Jaden - On My Own", hiphop, exponente, normal, moderna, solista, letras_rapidas).
 song("Run-DMC - Its Tricky", hiphop, exponente, normal, ochentas, banda, letras_rapidas).
 song("The Lonely Island - Im on a Boat (Ft. T-Pain)", hiphop, exponente, corta, dosmil, banda, letras_rapidas).
 song("The Lonely Island - Like A Boss", hiphop, exponente, corta, dosmil, banda, letras_rapidas).
-
-/* ELECTRONICA */
+% ----------------------------------------------ELECTRONICA----------------------------------------------
 song("Skrillex - Rock n Roll", electronica, exponente, larga, moderna, solista, sonidos_electronicos).
 song("Skrillex - Scary Monsters and Nice Sprites", electronica, exponente, larga, moderna, solista, sonidos_electronicos).
 song("Martin Garrix - Animals", electronica, exponente, larga, moderna, solista, sonidos_electronicos).
@@ -86,16 +81,14 @@ song("LMFAO - Party Rock Anthem", electronica, exponente, larga, moderna, banda,
 song("New Order - Elegoia", electronica, exponente, larga, ochentas, banda, sonidos_electronicos).
 song("Low Roar - Ill Keep Coming", electronica, exponente, larga, moderna, banda, sonidos_electronicos).
 song("Hello Venus - Im Ill", electronica, underground, normal, moderna, banda, sonidos_electronicos).
-
-/* Cumbia */
+% --------------------------------------------------CUMBIA-----------------------------------------------
 song("Supermerk2 - La resaka", cumbia, exponente, normal, dosmil, banda, ritmos_movidos).
 song("Chico Trujillo - Conductor", cumbia, exponente, corta, dosmil, banda, ritmos_movidos).
 song("Los Vikings 5 - Boquita de Caramelo", cumbia, exponente, normal, dosmil, banda, ritmos_movidos).
 song("Noche de Brujas - Debería Odiarte", cumbia, exponente, normal, dosmil, banda, ritmos_movidos).
 song("Luisko y la Fruta Fresca - Irme a Neptuno", cumbia, underground, larga, moderna, banda, ritmos_movidos).
 song("Leo Rey - Mortal Kumbiat", cumbia, exponente, normal, moderna, solista, ritmos_movidos).
-
-/* Clasica (8) */
+% ------------------------------------------------CLASICA------------------------------------------------
 song("Ludiwg Beethoven - Sonata per il piano n°12 Quasi una Fantasia. Opera 27,. N°2", clasica, exponente, larga, antigua, banda, sinfonia).
 song("Georg Handel - Messiah HWV 53", clasica, exponente, larga, antigua, banda, sinfonia).
 song("Amadeus Mozart - Serenade No. 13 for strings in G major, K. 525", clasica, exponente, larga, antigua, banda, sinfonia).
@@ -104,26 +97,54 @@ song("Edvard Grieg - Peer Gynt Opus 23", clasica, exponente, larga, antigua, ban
 song("Gioachino Rossini - William Tell overture", clasica, exponente, larga, antigua, banda, sinfonia).
 song("Bach - Cello suite No. 4 in E-flat major", clasica, exponente, larga, antigua, solista, sinfonia).
 song("Gaspar Cassado - Suite for Cello Solo", clasica, exponente, larga, antigua, solista, sinfonia).
-
-/* Country */
+%% -------------------------------------------------COUNTRY------------------------------------------------
 song("John Denver - Take Me Home, Country Roads", country, exponente, normal, setentas, solista, armonias_vocales).
 song("Dolly Parton - Jolene", country, underground, corta, setentas, solista, armonias_vocales).
-song("Willie Nelson - Always In My Mind", country, exponente, normal, , solista, armonias_vocales).
+song("Willie Nelson - Always In My Mind", country, exponente, normal, noventas, solista, armonias_vocales).
 song("Carter Family - Will The Circle Be Unbroken", country, underground, normal, antigua, banda, armonias_vocales).
 song("Patsy Montana - I Wanna Be A Cowboy Sweetheart", country, exponente, normal, antigua, solista, armonias_vocales).
-
-
-
-
-/* Techno */
-song("Green Velvet - Flash", techno, underground, larga, noventas, solista, tempo_rapido)
-song("Adam Beyer & Bart Skils - Your Mind", techno, underground, larga, moderna, banda, tempo_rapido)
-song("Niels Van Gogh - Pulverturm (Denise schneider remix)", techno, underground, larga, moderna, solista, tempo_rapido)
-song("D.K..O x KNNY x SHAW - I Feel", techno, underground, corta, moderna, banda, tempo_rapido)
-song(nombre, techno, tipoArt, duracion, decada, solis-banda, tempo_rapido)
-song(nombre, techno, tipoArt, duracion, decada, solis-banda, tempo_rapido)
-
-
-/* Blues */
-
-song("John Lee Hooker - Boom Boom", blues, exponente, larga, )
+song("Brad Paisley - Whiskey Lullaby", country, exponente, larga, dosmil, solista, armonias_vocales).
+song("The Highwaymen - Highwayman", country, exponente, normal, ochentas, banda, armonias_vocales).
+% -------------------------------------------------TECHNO------------------------------------------------
+song("Green Velvet - Flash", techno, underground, larga, noventas, solista, tempo_rapido).
+song("Adam Beyer & Bart Skils - Your Mind", techno, underground, larga, moderna, banda, tempo_rapido).
+song("Niels Van Gogh - Pulverturm (Denise schneider remix)", techno, underground, larga, moderna, solista, tempo_rapido).
+song("D.K..O x KNNY x SHAW - I Feel", techno, underground, corta, moderna, banda, tempo_rapido).
+song("Tiesto and Sneaky Sound System - I Will Be Here", techno, exponente, larga, dosmil, banda, tempo_rapido).
+song("Fisher - Losing It", techno, exponente, larga, moderna, solista, tempo_rapido).
+% ------------------------------------------------REGGAE------------------------------------------------
+song("Bob Marley - One Love", reggae, exponente, corta, setentas, solista, sonidos_bateria).
+song("Toots And The Maytals - Sweet And Dandy", reggae, exponente, normal, antigua, banda, sonidos_bateria).
+song("Junior Morvin - Police And Thieves", reggae, exponente, normal, setentas, banda, sonidos_bateria).
+song("Burning Spear – Slavery Days", reggae, underground, normal, setentas, solista, sonidos_bateria).
+song("Bob Andy - Life", reggae, exponente, normal, antigua, solista, sonidos_bateria).
+song("Gregory Isaacs - Words Of The Farmer", reggae, exponente, larga, setentas, solista, sonidos_bateria).
+song("Fred Locks - Love And Only Love", reggae, underground, larga, dosmil, solista, sonidos_bateria).
+% --------------------------------------------------SALSA-----------------------------------------------
+song("Celia Cruz y Johnny Pachecho - Quimbara", salsa, exponente, larga, setentas, banda, patron_cubano).
+song("Celia Cruz - La Vida Es Un Carnaval", salsa, exponente, larga, noventas, solista, patron_cubano).
+song("Marc Anthony - Ahora Quien (Salsa Version)", salsa, exponente, larga, dosmil, solista, patron_cubano).
+song("Coco Lagos y sus Orates - Guajirate ", salsa, underground, corta, noventas, banda, patron_cubano).
+song("Ismael Miranda y Orchestra Harlow - Abran Paso", salsa, underground, normal, setentas, banda, patron_cubano).
+song("Yiyo Sarante - Corazon de Acero", salsa, exponente, normal, moderna, solista, patron_cubano).
+% -------------------------------------------------ROMANTICA---------------------------------------------
+song("Lionel Richie, Diana Ross - Endless love", romance, exponente , larga, noventas, solista, letras_de_amor).
+song("Angelo Escobar - El marfil en su elefante", romance, underground, normal, moderna, solista, letras_de_amor).
+song("Genesis - Follow you follow me", romance, exponente, normal, setentas, banda, letras_de_amor).
+song("Porcupine tree - Trains", romance, exponente, larga, dosmil, banda, letras_de_amor).
+song("Glup! - Mi destino", romance, exponente, normal, dosmil, banda, letras_de_amor).
+song("Neutral Milk Hotel - In the aeroplane over the sea", romance, exponente, normal, noventas, banda, letras_de_amor).
+% -----------------------------------------------ROCK N ROLL----------------------------------------------
+song("Bill halley and his comets - Rock around the clock", rock_n_roll, exponente, corta, clasica, banda, ritmo_bailable_frenetico).
+song("Elvis Presley - Burning love", rock_n_roll, exponente, corta, setentas, solista, ritmo_bailable_frenetico).
+song("Chuck Berry - Johnny B. Goode", rock_n_roll, exponente, corta, antigua, solista, ritmo_bailable_frenetico).
+song("Chuck Berry - Roll Over Beethoven", rock_n_roll, exponente, corta, antigua, solista, ritmo_bailable_frenetico).
+song("The Ramblers - Rock del mundial", rock_n_roll, exponente, corta, antigua, banda, ritmo_bailable_frenetico).
+song("Yhe Beach Boys - Barbara Ann", rock_n_roll, exponente, corta, setentas, banda, ritmo_bailable_frenetico).
+% ---------------------------------------------------BLUES------------------------------------------------
+song("John Lee Hooker - Boom Boom", blues, exponente, larga, antigua, solista, tecnicas_expresivas).
+song("Stevie Ray Vaughan - Pride and Joy", blues, exponente, media, ochentas, solista, tecnicas_expresivas).
+song("The beatles - Yer Blues", blues, exponente, larga, antigua, banda, tecnicas_expresivas).
+song("B.B. King - The Thrill is Gone", blues, exponente, larga, antigua, solista, tecnicas_expresivas).
+song("Led Zeppelin - I cant Quit You Baby", blues, exponente, larga, antigua, banda, tecnicas_expresivas).
+song("Metro pa Quilpue - La Bkues Willis", blues, underground, larga, moderna, banda, tecnicas_expresivas).
